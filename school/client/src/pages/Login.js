3e24b7739c85709b7125aaf7ff5a8f05618dde3f
Amin <aminrafiq203@gmail.com>
2021-12-01 01:24:18 +0500

    Applying styles

--- school/client/src/pages/Login.js
+++ school/client/src/pages/Login.js
@@ -1,33 +1,89 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 import InputField from "../components/InputField";
 
 const Login = () => {
+  const [enteredLogin, setLogin] = useState({
+    email: "",
+    password: "",
+  });
 
-    const [enteredLogin, setLogin] = useState({
-        fullname: '',
-        password: ''
-    });
+  const handleChange = (e) => {
+    setLogin({ ...enteredLogin, [e.target.name]: e.target.value });
+  };
 
-    const handleChange = (e) => {
-        setLogin({ ...enteredLogin, [e.target.name]: e.target.value });
-    }
+  const submitForm = (e) => {
+    e.preventDefault();
+    setLogin({ email: "", password: "" });
+  };
 
-    const submitForm = (e) => {
-        e.preventDefault();
-        setLogin({ fullname: '', password: '' });
-    }
+  return (
+    <div className="hold-transition login-page">
+      <div className="login-box">
+        <div className="card card-outline card-primary">
+          <div className="card-header text-center">
+            <h1>
+              <b>My</b>School
+            </h1>
+          </div>
+          <div className="card-body">
+            <form onSubmit={submitForm}>
+              <div className="input-group mb-3">
+                <InputField
+                  onChange={handleChange}
+                  input={{
+                    id: "email",
+                    name: "email",
+                    type: "email",
+                    placeholder: "Email",
+                    value: enteredLogin.email,
+                    className: "form-control",
+                  }}
+                />
+                <div className="input-group-append">
+                  <div className="input-group-text">
+                    <span className="fas fa-envelope"></span>
+                  </div>
+                </div>
+              </div>
+              <div className="input-group mb-3">
+                <InputField
+                  onChange={handleChange}
+                  input={{
+                    id: "password",
+                    name: "password",
+                    type: "password",
+                    placeholder: "Password",
+                    value: enteredLogin.password,
+                    className: "form-control",
+                  }}
+                />
+                <div className="input-group-append">
+                  <div className="input-group-text">
+                    <span className="fas fa-lock"></span>
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <button type="submit" className="btn btn-primary btn-block">
+                  Sign In
+                </button>
+              </div>
+            </form>
+            <p className="mb-1">
+              <Link to="/forgotpassword">I forgot my password</Link>
+            </p>
+            <p className="mb-0">
+              <Link to="/register" className="text-center">
+                Register a new membership
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <form onSubmit={submitForm}>
-            <InputField onChange={handleChange} input={{ id: 'fullname', name: 'fullname', value: enteredLogin.fullname }} />
-            <InputField onChange={handleChange} input={{ id: 'password', name: 'password', value: enteredLogin.password }} />
-            <input type="submit" />
-            <Link to='/register'>Register</Link>
-            <Link to='/forgotpassword'>Forgot Password</Link>
-        </form>
-    );
-}
-
-export default Login
+export default Login;
--- school/client/src/pages/Register.js
+++ school/client/src/pages/Register.js
@@ -1,32 +1,175 @@
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 import InputField from "../components/InputField";
 
 const Register = () => {
+  const [enteredRegister, setRegister] = useState({
+    fullname: "",
+    email: "",
+    password: "",
+    repassword: "",
+  });
 
-    const state = useState({
-        fullname: "",
-        email: "",
-        password: ""
-    });
+  const [errorState, setLoginErrors] = useState({
+    fullnameError: "Please enter fullname",
+    emailError: "Please enter email",
+    passwordError: "",
+    repasswordError: "",
+  });
 
-    const submitForm = (e) => {
-        e.preventDefault();
-        console.log(e.target.fullname.value)
+  const handleChange = (e) => {
+    setRegister({ ...enteredRegister, [e.target.name]: e.target.value });
+  };
 
+  const submitForm = (e) => {
+    e.preventDefault();
+    const fullname = enteredRegister.fullname;
+    const email = enteredRegister.email;
+    const password = enteredRegister.password;
+    const repassword = enteredRegister.repassword;
+
+    if (
+      fullname !== "" &&
+      email !== "" &&
+      password !== "" &&
+      repassword !== ""
+    ) {
+      console.log("form posted");
+      setRegister({ fullname: "", email: "", password: "", repassword: "" });
+    } else {
     }
+  };
 
+  return (
+    <div className="hold-transition register-page">
+      <div className="register-box">
+        <div className="card card-outline card-primary">
+          <div className="card-header text-center">
+            <h1>
+              <b>My</b>School
+            </h1>
+          </div>
+          <div className="card-body">
+            <p className="login-box-msg">Register a new membership</p>
+            <form onSubmit={submitForm} autoComplete="off">
+              <div className="formInputBlock">
+                <div className="input-group">
+                  <InputField
+                    onChange={handleChange}
+                    input={{
+                      type: "text",
+                      id: "fullname",
+                      name: "fullname",
+                      defaultValue: enteredRegister.fullname,
+                      className: "form-control",
+                      placeholder: "Full name",
+                    }}
+                  />
+                  <div className="input-group-append">
+                    <div className="input-group-text">
+                      <span className="fas fa-user"></span>
+                    </div>
+                  </div>
+                </div>
+                {errorState.fullnameError && (
+                  <div
+                    style={{
+                      fontSize: 14,
+                      color: "red",
+                      paddingLeft: "4px",
+                    }}
+                  >
+                    {errorState.fullnameError}
+                  </div>
+                )}
+              </div>
 
-    return (
-        <form onSubmit={submitForm}>
-            <InputField input={{ type: 'text', id: 'fullname', name: 'fullname', defaultValue: state.fullname }} />
-            <InputField input={{ type: 'email', id: 'email', name: 'email', defaultValue: state.email }} />
-            <InputField input={{ type: 'password', id: 'password', name: 'password', defaultValue: state.password }} />
-            <Link to='/'>Login</Link>
-            <input type="submit" />
-        </form>
-    );
-}
+              <div className="formInputBlock">
+                <div className="input-group">
+                  <InputField
+                    onChange={handleChange}
+                    input={{
+                      type: "email",
+                      id: "email",
+                      name: "email",
+                      defaultValue: enteredRegister.email,
+                      className: "form-control",
+                      placeholder: "Email",
+                    }}
+                  />
+                  <div className="input-group-append">
+                    <div className="input-group-text">
+                      <span className="fas fa-envelope"></span>
+                    </div>
+                  </div>
+                </div>
+                {errorState.emailError && (
+                  <div
+                    style={{
+                      fontSize: 14,
+                      color: "red",
+                      paddingLeft: "4px",
+                    }}
+                  >
+                    {errorState.emailError}
+                  </div>
+                )}
+              </div>
 
-export default Register
+              <div className="formInputBlock">
+                <div className="input-group">
+                  <InputField
+                    onChange={handleChange}
+                    input={{
+                      type: "password",
+                      id: "password",
+                      name: "password",
+                      defaultValue: enteredRegister.password,
+                      className: "form-control",
+                      placeholder: "Password",
+                    }}
+                  />
+                  <div className="input-group-append">
+                    <div className="input-group-text">
+                      <span className="fas fa-lock"></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="input-group mb-3">
+                <InputField
+                  onChange={handleChange}
+                  input={{
+                    type: "password",
+                    id: "repassword",
+                    name: "repassword",
+                    defaultValue: enteredRegister.repassword,
+                    className: "form-control",
+                    placeholder: "Retype Password",
+                  }}
+                />
+                <div className="input-group-append">
+                  <div className="input-group-text">
+                    <span className="fas fa-lock"></span>
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <button type="submit" className="btn btn-primary btn-block">
+                  Register
+                </button>
+              </div>
+            </form>
+            <Link to="/" className="text-center">
+              I already have a membership
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
